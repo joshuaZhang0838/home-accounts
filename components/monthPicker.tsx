87cde3300da463a14d6d2998a8aec31b357c9128
joshuaZhang0838 <cosilon@yahoo.com.tw>
2023-01-06 15:02:30 +0800

    api測試中

--- components/monthPicker.tsx
+++ components/monthPicker.tsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { subMonths } from 'date-fns';
+import { subMonths, getMonth } from 'date-fns';
 
 interface dateFuncProps {
   onSelectDate: (date: Date) => void;
 }
 
-const MonthPicker = (props: dateFuncProps) => {
+const MonthPicker = ({ onSelectDate }: dateFuncProps) => {
   const [minDate, setMinDate] = useState<Date>();
   const [maxDate, setMaxDate] = useState<Date>();
   const [selectDate, setSelectDate] = useState<Date>(new Date());
-  //const [selectDate, setSelectDate] = useState<Date>();
 
   const setInitialDate = () => {
     const now = new Date();
@@ -22,25 +21,20 @@ const MonthPicker = (props: dateFuncProps) => {
   useEffect(() => {
     setInitialDate();
   }, []);
-  // useEffect(() => {
-  //   props.onSelectDate(selectDate as Date);
-  // }, [selectDate]);
   return (
     <DatePicker
+      openTo="month"
       views={['year', 'month']}
       label="Year and Month"
+      inputFormat="yyyy/MM"
       minDate={minDate}
       maxDate={maxDate}
       value={selectDate}
-      onChange={(newDate) => {
-        console.log(`newDate is ${newDate}`);
-        setSelectDate(newDate as Date);
+      onMonthChange={(newDate) => {
+        setSelectDate(newDate);
+        onSelectDate(newDate);
       }}
-      // onClose={() => {
-      //   console.log(`selectDate is ${selectDate}`);
-      //   props.onSelectDate(selectDate as Date);
-      //   //setSelectDate(tempDate);
-      // }}
+      onChange={() => {}}
       renderInput={(params) => <TextField {...params} helperText={null} />}
     />
   );
--- pages/invoice.tsx
+++ pages/invoice.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, Fragment } from 'react';
 import { Box } from '@mui/material';
+import useSWR from 'swr';
 import axios from 'axios';
+
 import MonthPicker from '../components/monthPicker';
 import { getYear, getMonth } from 'date-fns';
 
@@ -8,14 +10,17 @@ export default function Invoice() {
   //const [date, setDate] = useState<Date>(new Date());
   const [st, setSt] = useState<string>('');
 
-  const fetchUser = async (date: Date) => {
-    //const ff = await axios.get('/api/invoices');
-    //console.log(ff);
+  const fetchUser =  (date: Date) => {
+    // const fetcher = (url:string) => axios.get<string>(url).then(res => res.data)
+    // let url = '/api/invoices';
+    // const ff = useSWR(url,fetcher);
+    // console.log(ff);
     //setSt(ff.data.name);
     let year = getYear(date);
-    let month = getMonth(date);
+    let month = getMonth(date) + 1;
     console.log(`the year is ${year}`);
     console.log(`the month is ${month}`);
+    
   };
   // useEffect(() => {ya
   //   fetchUser();
