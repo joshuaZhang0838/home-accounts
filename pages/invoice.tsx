7aa4d30053e12a21aae67b7fc24673b904e711fe
Joshua0838 <cosilon@yahoo.com.tw>
2023-01-07 00:32:34 +0800

    完成SWR測試

--- pages/invoice.tsx
+++ pages/invoice.tsx
@@ -1,27 +1,26 @@
 import { useState, useEffect, Fragment } from 'react';
 import { Box } from '@mui/material';
-import useSWR from 'swr';
+import useSWR, { Key, Fetcher } from 'swr';
+import type { Data } from '../pages/api/invoices';
 import axios from 'axios';
 
 import MonthPicker from '../components/monthPicker';
 import { getYear, getMonth } from 'date-fns';
 
 export default function Invoice() {
-  //const [date, setDate] = useState<Date>(new Date());
   const [st, setSt] = useState<string>('');
-
-  const fetchUser =  (date: Date) => {
-    // const fetcher = (url:string) => axios.get<string>(url).then(res => res.data)
-    // let url = '/api/invoices';
-    // const ff = useSWR(url,fetcher);
-    // console.log(ff);
-    //setSt(ff.data.name);
+  const fetcher: Fetcher<Data, string> = (url: string) =>
+    fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR('/api/invoices', fetcher);
+  const fetchUser = (date: Date) => {
     let year = getYear(date);
     let month = getMonth(date) + 1;
     console.log(`the year is ${year}`);
     console.log(`the month is ${month}`);
-    
   };
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>error</div>;
+
   // useEffect(() => {ya
   //   fetchUser();
   // }, [date]);
@@ -30,7 +29,8 @@ export default function Invoice() {
       <Box sx={{ ml: 3, mt: 3 }}>
         <MonthPicker onSelectDate={fetchUser} />
       </Box>
-      <Box>{st}</Box>
+
+      <div>{data?.name}</div>
     </Fragment>
   );
 }
